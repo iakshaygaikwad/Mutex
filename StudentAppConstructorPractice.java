f755ccff01403f612b64ce98bbd6e2625659955e
Akshay Gaikwad <iakshaygaikwad@gmail.com>
2017-07-18 21:41:54 +0530

    Removed unused comments and methods private constructor example

--- StudentAppConstructorPractice.java
+++ StudentAppConstructorPractice.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Constructor;
 
 public class StudentAppConstructorPractice{
 
--- StudentConstructorPractice.java
+++ StudentConstructorPractice.java
@@ -1,27 +1,13 @@
 
 public class StudentConstructorPractice  {
 
-
-    private String name = "";
     private static StudentConstructorPractice s = new StudentConstructorPractice();
      private StudentConstructorPractice()
     {
         System.out.println("Default Constructor....");
     }
-//    private StudentConstructorPractice(String name)
-//    {
-//        this.name = name;
-//    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static Object getObject()
+     public static Object getObject()
     {
         return s;
     }
