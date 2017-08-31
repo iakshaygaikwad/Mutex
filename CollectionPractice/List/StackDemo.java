4cc0626e061d7d504ed26d3ea166693cdb9727ca
Akshay Gaikwad <iakshaygaikwad@gmail.com>
2017-08-31 19:11:27 +0530

    Stack and Vector Practice

---
+++ CollectionPractice/List/StackDemo.java
@@ -0,0 +1,34 @@
+package CollectionPractice.List;
+
+import java.util.Enumeration;
+import java.util.Stack;
+import java.util.Vector;
+
+public class StackDemo{
+        public static void printer(Vector vector)
+        {
+            Enumeration enumeration = vector.elements();
+            while (enumeration.hasMoreElements())
+                System.out.println(enumeration.nextElement());
+        }
+    public static void main(String[] args) {
+            Stack stack = new Stack();
+            for (int i = 1; i < 10; i++)
+            {
+                stack.push(i);
+            }
+            ALDemo.printer(stack);
+
+        System.out.println("Size of stack = "+ stack.size() + " Capacity of Stack = "+stack.capacity());
+        stack.push(10);
+        stack.push(11);
+        System.out.println("Size of stack = "+ stack.size() + " Capacity of Stack = "+stack.capacity());
+
+       // System.out.println(stack.pop());
+        //pop operation
+        while (!stack.isEmpty())
+        {
+            System.out.println(stack.pop() + " POP");
+        }
+    }
+}
---
+++ CollectionPractice/List/VectorDemo.java
@@ -0,0 +1,21 @@
+package CollectionPractice.List;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class VectorDemo {
+
+    public static void printer(Vector vector)
+    {
+        Enumeration enumeration = vector.elements();
+        while (enumeration.hasMoreElements())
+            System.out.println(enumeration.nextElement());
+    }
+    public static void main(String[] args) {
+        Vector vector = new Vector();
+        for ( int i = 1; i <= 10; i++)
+            vector.addElement(i);
+        VectorDemo.printer(vector);
+        System.out.println("capacity = " + vector.capacity());
+    }
+}
